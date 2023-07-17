7a935da796fdc71a9de6ccac739121c21b0e88c6
Daniel Olivieri <dfolivieri@gmail.com>
2023-07-17 11:57:31 -0400

    Now have words there
    But it's still janky

--- src/app/phrases/philosophy-death/philosophy-death.component.ts
+++ src/app/phrases/philosophy-death/philosophy-death.component.ts
@@ -1,4 +1,4 @@
-import { Component, ElementRef, OnInit, ViewChild } from '@angular/core';
+import { Component, ElementRef, HostListener, OnInit, ViewChild } from '@angular/core';
 
 /*
 
@@ -20,15 +20,127 @@ export class PhilosophyDeathComponent implements OnInit {
 
   constructor() { }
 
-  @ViewChild('philosophy') box!: ElementRef;
+  @ViewChild('philosophyBoxId') box!: ElementRef;
+  @ViewChild('quoteId') quote!: ElementRef;
+
+  height = 100;
+  width = 100;
+  top = 0;
+  left = 0;
+  quoteCount = 0;
+
+  quoteArray = [
+    {text: 'To study philosophy is to learn to die.',
+    fontSize: 10,
+    x: 0,
+    y: 20
+  },
+    {text: 'We die a little bit every day',
+    fontSize: 8,
+    x: 30,
+    y: 20
+  }, {text: 'We cannot practice death',
+  fontSize: 6,
+  x: 10,
+  y: 10
+},
+  ]
+
+  currentQuote = this.quoteArray[0];
 
   ngOnInit(): void {
   }
 
+  @HostListener('document:keydown', ['$event']) 
+  onKeydown($event: KeyboardEvent) {
+    console.log($event.key);
+    if ($event.key == 'ArrowRight') {
+      console.log($event.key);
+      this.advanceTopAndLeft();
+      this.advanceHeightAndWidth();
+      this.positionText();
+      this.changeMainCanvasSize();
+    } else if ($event.key == 'ArrowLeft') {
+      console.log($event.key);
+      this.retractTopAndLeft();
+      this.retractHeightAndWidth();
+      this.changeMainCanvasSize();
+    }
+  };
 
-  changeMainCanvasSize(heightChangeAmount: number, widthChangeAmount: number) {
-    console.log(this.box.nativeElement.getBoundingClientRect())
+  changeHeight(amount: number) {
+    this.height += amount;
+  }
+  changeWidth(amount: number) {
+    this.width += amount; 
+  }
+  changeLeft(amount: number) {
+    this.left += amount;
+  }
+  changeTop(amount: number) {
+    this.top += amount; 
+  }
 
+  advanceHeightAndWidth() {
+    if (this.height > 30) {
+      this.height -= 5;
+    } else if (this.height > 10) {
+      this.height -= 3;
+    }
+
+    if (this.width > 30) {
+      this.width -= 5;
+    } else if (this.width > 10) {
+      this.width -= 3;
+    }
+  }
+
+    retractHeightAndWidth() {
+      if (this.height < 30) {
+        this.height += 5;
+      } else if (this.height < 10) {
+        this.height += 3;
+      }
+  
+      if (this.width < 30) {
+        this.width += 5;
+      } else if (this.width < 10) {
+        this.width += 3;
+      }
+
+  }
+
+  advanceTopAndLeft() {
+    if (this.top < 30) {
+      this.top += 5;
+    }
+    if (this.left < 30) {
+      this.left += 5;
+    }
+  }
+
+  retractTopAndLeft() {
+    if (this.top >= 10) {
+      this.top -= 5;
+    }
+
+    if (this.left >= 10) {
+      this.left -= 5;
+    }
+  }
+
+  changeMainCanvasSize() {
+    console.log(this.box.nativeElement.getBoundingClientRect());
+    this.box.nativeElement.setAttribute(`style`, `top: ${this.top}%; left: ${this.left}%; height: ${this.height}vh; width: ${this.width}vw` );
+  
+  }
+
+  positionText() {
+    console.log(this.box.nativeElement.getBoundingClientRect());
+    console.log(this.quote.nativeElement.getBoundingClientRect());
+    this.quote.nativeElement.setAttribute('style', `font-size: ${this.currentQuote.fontSize}em`)
+    this.quoteCount++;
+    this.currentQuote = this.quoteArray[this.quoteCount];
   }
 
 }
